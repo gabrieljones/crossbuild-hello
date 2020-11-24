7b0b3f60153821a1df5e4e1fed43a2396f768926
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2020-11-24 18:47:44 -0500

    repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
   scala
 }
 
+repositories {
+  google()
+  jcenter()
+  mavenCentral()
+}
+
 dependencies {
   implementation("org.scala-lang:scala-library:2.13.3")
   testImplementation("org.scalatest:scalatest_2.13:3.2.1")
